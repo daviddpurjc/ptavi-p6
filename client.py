cf52e8a636ca567973490795006506f1b90b0b4a
daviddp <david.duran.prieto@gmail.com>
2015-11-30 10:59:37 +0100

    commitlunes

--- client.py
+++ client.py
@@ -5,26 +5,54 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
-# Cliente UDP simple.
+# Cliente UDP
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+if not len(sys.argv) == 3:
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+if sys.argv[1] != 'INVITE' and sys.argv[1] != 'BYE':
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
+
+if str(sys.argv[2]).find('@') == -1:
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
+
+if str(sys.argv[2]).find(':') == -1:
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
+
+
+METODO = sys.argv[1]
+cadena = str(sys.argv[2])
+LOGIN = cadena[:cadena.find('@')]
+IP = cadena[cadena.find('@')+1:cadena.find(':')] 
+PUERTO = int(cadena[cadena.find(':')+1:])
+
+LINE = METODO+ ' sip:'+LOGIN+'@'+IP+' SIP/2.0\r\n'
+LINE2 = 'ACK sip:'+LOGIN+'@'+IP+' SIP/2.0\r\n'
+LINE3 = 'BYE sip:'+LOGIN+'@'+IP+' SIP/2.0\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((IP, PUERTO))
 
 print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
 
 print('Recibido -- ', data.decode('utf-8'))
+
+if data.decode('utf-8')=="SIP/2.0 100 Trying\r\nSIP/2.0 180 Ring\r\nSIP/2.0 200 OK\r\n\r\n":
+    print("Enviando: " + LINE2)
+    my_socket.send(bytes(LINE2, 'utf-8') + b'\r\n')
+    #my_socket.listen(0)
+    data = my_socket.recv(5120)
+    #data.listen(0)
+    print("Enviando: " + LINE3)
+    my_socket.send(bytes(LINE3, 'utf-8') + b'\r\n')
+    data = my_socket.recv(1024)
+###    $cvlc rtp://@127.0.0.1:23032
 print("Terminando socket...")
 
 # Cerramos todo
--- server.py
+++ server.py
@@ -5,27 +5,47 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import os
+import sys
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
+    IP = sys.argv[1]
 
     def handle(self):
+        """ Metodo principal del servidor. """
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
-
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
+        print (self.client_address[0])
+        print (self.client_address[1])
+        line = self.rfile.read()
+        print("El cliente nos manda " + line.decode('utf-8'))
+        deco = line.decode('utf-8')
+        if deco.startswith('INVITE'):
+            self.wfile.write(b"SIP/2.0 100 Trying\r\nSIP/2.0 180 Ring\r\nSIP/2.0 200 OK\r\n\r\n")
+        elif deco.startswith('ACK'):
+            fichero_audio = str(sys.argv[3])
+            aEjecutar = "./mp32rtp -i 127.0.0.1 -p 23032 < " + fichero_audio
+            #aEjecutar = "./mp32rtp -i "+str(self.client_address[0])+" -p "+str(self.client_address[1])+" < " + fichero_audio
+            print("Vamos a ejecutar: ")
+            print(aEjecutar)
+            os.system(aEjecutar)
+        elif deco.startswith('BYE'):
+            self.wfile.write(b"Cuelga tu, cuelgo yo")
+        else:
+            self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+
+    if len(sys.argv) != 4:
+        sys.exit("Usage: python server.py IP port audio_file")
+
+    try:
+        serv = socketserver.UDPServer(('', int(sys.argv[2])), SIPHandler)
+        print("Listening...")
+        serv.serve_forever()
+    except:
+        sys.exit("Usage: python server.py IP port audio_file")
